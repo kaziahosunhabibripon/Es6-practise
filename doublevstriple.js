2e8b2cf059ba3ad7b53065d8c58d4f2aa841af7f
Ripon <habibkazi92@gmail.com>
2021-02-02 01:20:23 +0800

    map and filter

---
+++ doublevstriple.js
@@ -0,0 +1,10 @@
+const first = 2;
+const last = "2";
+if (first === last){
+    console.log("True");
+}else{
+    console.log("False");
+}
+// == equal value "2" == 2, true did not check type of value or data type
+
+// === matching type exactly  
---
+++ map.js
@@ -0,0 +1,32 @@
+
+// const output = [];
+// for (let i =0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+// console.log(output);
+
+// function square(element){
+//     return element * element;
+// }
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+
+// numbers.map(function(element, index, array){
+//     console.log(element, index, array);
+// })
+// console.log(output);
+
+// const square = element => element * element;
+// const square = x => x *x;
+const numbers = [3 ,4 ,5, 6, 7, 8];
+// const result = numbers.map(x=> x* x);
+// console.log(result);
+const bigger = numbers.filter(x=> x >5);
+
+const isThere = numbers.find(x=> x<5);
+// forEach,reduce
+    
+console.log(isThere);
